50cb1f029bb16fba5d940c99afbf619e7edf76b6
johan antonio <mancebojohan97@gmail.com>
2024-11-27 17:38:33 -0400

    componente de enrutamiento

---
+++ src/componentes/Principal.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
+    );
+  }
+  
